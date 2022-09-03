f5c08738c877cfdc0fb0927c80bf04c9359f9ae1
Jay Thanki <thankijay99@gmail.com>
2022-09-03 17:19:11 +0100

    Learning Go

--- practice.go
+++ practice.go
@@ -1,11 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	// variables
 	// var name string = "Jay Thanki"
 	// var number int = 12
 
-	fmt.Println("Hi")
+	// fmt.Println("Hi")
+
+	// fmt
+	// fmt.Printf("Number: %d ", 2)
+	// fmt.Printf("Number: %b ", 2)
+	// fmt.Printf("Number: %x ", 2)
+
+	// user input
+	// scanner := bufio.NewScanner(os.Stdin)
+	// fmt.Printf("Enter the year you were born: ")
+	// scanner.Scan()
+	// input, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+	// fmt.Printf("You will be %d years old at the end of 2022 ", 2022-input)
+
+	// conditionals
+	// x := 5 < 7 || 6 > 10
+	// x := 5 < 7 && 6 > 10
+	// fmt.Print(x)
+
+	// if, else if, else
+	// age := 23
+
+	// if age >= 18 {
+	// 	fmt.Print("You can ride")
+	// } else if age >= 14 {
+	// 	fmt.Print("You can ride with a parent")
+	// } else {
+	// 	fmt.Print("You cannot ride")
+	// }
+
+	// loops
+	for i := 0; i <= 5; i++ {
+		fmt.Println(i)
+	}
+
+	// i := 0
+	// for i <= 5 {
+	// 	fmt.Println(i)
+	// 	i++
+	// }
+
 }
